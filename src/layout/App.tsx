26149b8a444a4d30d6d004ae217f5744c22a2933
Shufutinski <48769722+Shufutinski@users.noreply.github.com>
2023-06-22 13:07:07 +0300

    feat: prepare the foundation for the base of the calculator

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,8 +1,20 @@
 const App = () => {
   return (
     <>
-      <div className="">
-
+      <div className="inline-flex flex-wrap justify-center content-center w-screen h-screen">
+        <div className="bg-white inline-flex flex-wrap content-end w-1/3 h-2/3 border-2 border-zone shadow-2xl">
+          <div className="inline-flex flex-wrap justify-around content-around w-full h-2/3 border-t-2 border-zone">
+            <div className="button-digits">1</div>
+            <div className="button-digits">2</div>
+            <div className="button-digits">3</div>
+            <div className="button-digits">4</div>
+            <div className="button-digits">5</div>
+            <div className="button-digits">6</div>
+            <div className="button-digits">7</div>
+            <div className="button-digits">8</div>
+            <div className="button-digits">9</div>
+          </div>
+        </div>
       </div>
     </>
   );
